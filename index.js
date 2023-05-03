e366ffc6022accb54ac944b6ebb8a8dad8ec27a7
safatsaat2 <sakibsafat47@gmail.com>
2023-05-03 22:07:14 +0600

    chef data created and cors installed

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+const chefs = require('./chef.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chefs', (req, res)=>{
+    res.send(chefs)
 })
 
 app.listen(port, () => {
